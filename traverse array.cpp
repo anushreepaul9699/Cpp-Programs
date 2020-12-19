aab7bd5b12f83f1481bfe4464928d5f2e4e1298a
Anushree Paul <74617999+anushreepaul9699@users.noreply.github.com>
2020-12-20 01:26:59 +0530

    Add files via upload

---
+++ traverse array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+	int i,n;
+	int LA[]={1,3,5,7,8};
+	cout<<" value of n ";
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cout<<"LA["<<i<<"]"<<"="<<LA[i]<<endl;
+	}
+	return 0;
+}
